2b18b15db20a09da1def8616bdbf663252342ddb
DIEGO FARIAS <diegofariasilva123@gmail.com>
2023-04-03 18:12:42 -0300

    tabuada

---
+++ tabuada.py
@@ -0,0 +1,16 @@
+print('#'*20)
+print('Tabuada')
+print('#'*20)
+
+numero = int(input('Informe um número para saber sua TABUADA \n'))
+print('-'*20)
+print(f'A tabuada de {numero} é:')
+a = 0
+while a<10:
+    a+=1
+    multiplicar = numero*a    
+    print(f'=> {numero} x {a} = {multiplicar}')
+
+print('-'*20)
+
+
